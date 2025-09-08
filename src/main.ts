501a7ee90b39e4aafeae18128a9fa81b3d2eb399
Himasha Sandeepani <141569662+SANDEEPANI-RMH@users.noreply.github.com>
2025-09-08 11:35:03 +0530

    fix navigation issue(navigate to dashboard)

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,27 @@
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { App } from './app/app';
+// import { provideHttpClient } from '@angular/common/http'; 
+// import { provideRouter } from '@angular/router';
+// import { routes } from './app/app.routes';
+
+// bootstrapApplication(App, {
+//   providers: [
+//     provideRouter(routes),
+//     provideHttpClient()   //Register HttpClient globally
+//   ]
+// }).catch(err => console.error(err));
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
 import { provideHttpClient } from '@angular/common/http'; 
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
+// Clear logged-in user on app start
+localStorage.removeItem('currentUser');
+
 bootstrapApplication(App, {
   providers: [
     provideRouter(routes),
-    provideHttpClient()   //Register HttpClient globally
+    provideHttpClient()   // Register HttpClient globally
   ]
 }).catch(err => console.error(err));
